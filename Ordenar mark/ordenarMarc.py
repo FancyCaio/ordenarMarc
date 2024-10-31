5d237f828f472e4fe60b4986e853fdc8e44f9cd5
Caio Braga <127174343+FancyCaio@users.noreply.github.com>
2024-10-30 23:20:55 -0300

    Add files via upload

---
+++ Ordenar mark/ordenarMarc.py
@@ -0,0 +1,9 @@
+def ler_arquivo_e_ordenar(nome_arquivo):
+    with open(nome_arquivo, 'r') as arquivo:
+        conteudo = arquivo.read().strip()
+        numeros = list(map(int, conteudo.split()))
+        numeros_ordenados = sorted(numeros)
+        return ' '.join(map(str, numeros_ordenados))
+arquivo_nome = 'extra.txt'
+resultado = ler_arquivo_e_ordenar(arquivo_nome)
+print(f'Números ordenados: {resultado}')
